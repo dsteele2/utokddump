dee9a5f990854a6abeb2ded141015112f0c2f6f3
michael baker <MBaker1987@outlook.com>
2022-08-21 17:04:29 -0500

    Sort added to most columns

--- main.js
+++ main.js
@@ -1,30 +1,7 @@
 
-let NWsort = 'asc';
-function sortTableByColumnm(table,column,acs=true){
-const tBody=table.tBody;
-const rows=Array.from(tBody.querySelectorAll('tr'));
+let sortdir = 'asc';
 
-const sortedRows=rows.sort((a,b)=>{
-    const aColText=a.querySelector('td:nth-child(${column +1})').textContent.trim()
-    const bColText=b.querySelector('td:nth-child(${column +1})').textContent.trim()
-
-    return aColText>bColText ? (1+dirModifier):(-1 * dirModifier);
-
-});
-
-while(tBody.firstChild){
-    tBody.removeChild(tBody.firstChild);
-}
-tBody.append(...sortedRows);
-
-table.querySelectorAll("th").foreach(th=> th.classlist.remove('th-sort-asc','th-sort-desc'))
-
-}
-
-
-
-
-function buildTable(sortdirection='asc') {
+function buildTable(sortdirection='asc',sortCol='sortNW') {
  
   $(document).ready(function(){
   $.ajax({
@@ -35,18 +12,71 @@ function buildTable(sortdirection='asc') {
     
     var table = document.getElementById("myTable");
     table.innerHTML=""
-    response.sort(function(a, b) {
-      return parseFloat(a.nw) - parseFloat(b.nw)
-    })
+    console.log(response)
+
+    response=modify_data(response)
+
+    switch(sortCol){
+      case 'sortNW':
+        response.sort(function(a, b) {
+          return parseFloat(a.nw) - parseFloat(b.nw)
+        });
+      break;
+      case 'sortLoc':
+        response.sort(function(a, b) {
+          return parseFloat(a.ISLANDNUMBER) - parseFloat(b.ISLANDNUMBER) || parseFloat(a.KDNUMBER)- parseFloat(b.KDNUMBER)
+        });
+      break;
+      case 'sortLand':
+        response.sort(function(a, b) {
+          return parseFloat(a.land) - parseFloat(b.land)
+        });
+      break;
+      case 'sortProvs':
+        response.sort(function(a, b) {
+          return parseFloat(a.count) - parseFloat(b.count)
+        });
+      break;
+      case 'sortWW':
+        response.sort(function(a, b) {
+          return parseFloat(a.WARWINS) - parseFloat(b.WARWINS)
+        });
+      break;
+      case 'sortTW':
+        response.sort(function(a, b) {
+          return parseFloat(a.TOTALWARS) - parseFloat(b.TOTALWARS)
+        });
+      break;
+      case 'sortWinP':
+        response.sort(function(a, b) {
+          return parseFloat(a.WINPERCENT) - parseFloat(b.WINPERCENT)
+        });
+      break;
+      case 'sortStance':
+        response.sort(function(a, b) {
+          return a.stance - b.stance
+        });
+      break;
+
+
+      
+      
+
+    }
+  
     if(sortdirection=='desc'){
       response.reverse()
     }
 
     
     console.log(response);
+    response = modify_data(response);
+    console.log(response);
+    
 
     for (var i = 1; i < response.length - 1; i++) {
       var wars
+    
       var row = `<tr>
                         <td>${response[i].loc}</td>
                         <td>${response[i].name}</td>
@@ -54,23 +84,69 @@ function buildTable(sortdirection='asc') {
                         <td>${(response)[i].land.toLocaleString().concat(" acres")}</td>
                         <td>${response[i].count}</td>
                         <td>${response[i].stance}</td>
-                        <td>${response[i].wars}</td>
+                        <td>${response[i].WARWINS}</td>
+                        <td>${response[i].TOTALWARS}</td>
+                        <td>${(response)[i].WINPERCENT.toLocaleString().concat("%")}</td>
                   </tr>`;
       table.innerHTML += row;
      
     }
     
-    NWsort=(NWsort == 'asc' ? 'desc' : 'asc');
+    sortdir=(sortdir == 'asc' ? 'desc' : 'asc');
   },
 });
   })
 
-  
-
 }
-var element = document.getElementById('sortup');
-element.onclick = function() {buildTable(NWsort);};
-buildTable(NWsort)
+
+function modify_data(response=[]){
+  for (var i = 1; i < response.length - 1; i++) {
+    
+    response[i].KDNUMBER=response[i].loc.split(':')[1]
+    response[i].ISLANDNUMBER=response[i].loc.split(':')[0]
+    response[i].WARWINS=response[i].wars[0]
+    response[i].TOTALWARS=response[i].wars[1]
+    if (response[i].TOTALWARS==0){
+      var winP=0
+      winP=winP.toFixed(2)
+      response[i].WINPERCENT=winP
+        }
+    else{
+      response[i].WINPERCENT=(parseFloat(response[i].WARWINS)/parseFloat(response[i].TOTALWARS)*100).toFixed(2)
+    }
+    
+
+
+  }
+  return response
+}
+var sortNW = document.getElementById('sortNW');
+var sortLoc = document.getElementById('sortLoc');
+var sortLand = document.getElementById('sortLand');
+var sortProvs = document.getElementById('sortProvs');
+var sortWW = document.getElementById('sortWW');
+var sortTW= document.getElementById('sortTW');
+var sortWinP= document.getElementById('sortWinP');
+var sortStance = document.getElementById('sortStance');
+
+sortNW.onclick = function() {buildTable(sortdir,'sortNW');
+}
+sortLoc.onclick = function() {buildTable(sortdir,'sortLoc');
+}
+sortLand.onclick = function() {buildTable(sortdir,'sortLand');
+}
+sortProvs.onclick = function() {buildTable(sortdir,'sortProvs');
+}
+sortWW.onclick = function() {buildTable(sortdir,'sortWW');
+}
+sortWinP.onclick = function() {buildTable(sortdir,'sortWinP');
+}
+sortTW.onclick = function() {buildTable(sortdir,'sortTW');
+}
+sortStance.onclick = function() {buildTable(sortdir,'sortStance');
+}
+
+buildTable(sortdir,'sortNW')
 
 
 
